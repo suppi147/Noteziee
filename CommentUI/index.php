81c75920e53e62d3c3e64f5d0feb5c16dea61a4c
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-02-18 21:35:32 +0700

    expand body to comment section

--- CommentUI/index.php
+++ CommentUI/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <title>Noteziee</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -18,12 +19,13 @@
      <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
     </div>
    </form>
-</body>
 <?php
 include __DIR__.'/../CommentController/ShowComment.php';
 
 $showerPack= new ShowComment();
+
 foreach( $showerPack->Show() as $shower){
   echo $shower;
 }
+echo '</body>';
 ?>
