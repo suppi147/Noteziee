8694f882ffa5c9fc03ab16af007a4e99da062333
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-02-28 16:46:14 +0700

    add detele ability

---
+++ CommentController/DeleteComment.php
@@ -0,0 +1,39 @@
+<?php
+require_once("CommentController.php");
+require_once("FilterComment.php");
+class DeleteComment extends CommentController{
+    protected $filterNet;
+    protected $filterID;
+    function __construct(){
+    $this->filterNet= new FilterComment();
+	parent::__construct();
+    }
+
+    function Delete($unfilterID){
+        parent:$this->Connect2DB();
+        //id filtering
+        if($this->filterNet->FilterID($unfilterID)==true){
+            $this->filterID=$this->filterNet->GetItemfiltering();
+            //execute Delete query
+            $query="DELETE FROM CommentTable WHERE id=".$this->filterID;
+            $statement= $this->connect->prepare($query);
+            $statement->execute();
+        }
+        else{
+            echo 'invalid ID';
+        }
+        
+
+
+        //Announcing
+        //echo '<label class="text-fail"> Comment deleted</label>';
+
+        //Show again
+
+
+        //Disconnect DB
+        $this->Disconnect2DB();
+
+    }
+}
+?>
