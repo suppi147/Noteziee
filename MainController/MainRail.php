28e5ecd2bc15c2986ff3191c52958a55471954e6
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-01 13:41:16 +0700

    add delete control flag case

--- MainController/MainRail.php
+++ MainController/MainRail.php
@@ -1,15 +1,21 @@
 <?php
-    include __DIR__.'CommentController/CommentController.php';
-    include __DIR__.'CommentController/PostComment.php';
+include __DIR__.'/../CommentController/CommentController.php';
+include __DIR__.'/../CommentController/PostComment.php';
+include __DIR__.'/../CommentController/DeleteComment.php';
 
 $controller= new CommentController();
 
 if(strcmp($_POST["control_flag"],"post")==0){
      $postTrigger=new PostComment();
-     $postTrigger->post($_POST["commentContent"]);
+     $postTrigger->Post($_POST["commentContent"]);
+}
+else if(strcmp($_POST["control_flag"],"delete")==0){
+     $postTrigger=new DeleteComment();
+     $postTrigger->Delete($_POST["id"]);
 }
 else{
-     echo "control flag invalid";
+     echo "error";
      header("Location: ".$controller->indexLocation);
 }
+header("Location: ".$controller->indexLocation);
 ?>
