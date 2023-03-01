c52ada6e1b227749c95e2cf994bff1ddbab3cccc
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-01 22:20:11 +0700

    add extended comment function

--- CommentUI/index/index.php
+++ CommentUI/index/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
 
-        <title>project cards - Bootdey.com</title>
+        <title>Noteziee ;3</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet" />
@@ -12,15 +12,15 @@
         <script src="http://localhost/Noteziee/CommentUI/js/index.js"></script>
     </head>
     <body>
-        <div class="container p-0">
+        <div class="container">
             <h1 class="h1-edit">Noteziee</h1>
             <form action="http://localhost/Noteziee/MainController/MainRail.php" method="POST" id="commentID">
                 <div class="form-group">
-                 <textarea name="commentContent" id="commentContent" class="form-control" placeholder="Enter Comment" rows="6" cols="40"></textarea>
+                 <textarea name="commentContent" id="commentContent" class="form-control" placeholder="Enter Comment" rows="10" cols="60"></textarea>
                 </div>
                 <div class="form-group">
                  <input type="hidden" name="control_flag" id="control_flag" value="post" />
-                 <div class="col text-center"><input type="submit" name="submit" id="submit" class="btn btn-primary " value="Submit" />
+                 <div class="col text-center"><input type="submit" name="submit" id="submit" class="btn btn-primary btn-lg" value="Submit" />
                 </div> 
                 </div>
             </form>
@@ -28,52 +28,68 @@
 <?php
 include __DIR__.'/../../CommentController/FetchComment.php';
 include __DIR__.'/../../CommentController/DeleteComment.php';
-$htmlStarter1='<div class="col-12 col-md-6 col-lg-3">
+
+define("MAX_COMMENTLINE",6);
+$CommentBoxStarter='<div class="col-12 col-md-6 col-lg-4">
 <div class="card">
     <div class="card-header px-4 pt-4">
         <div class="card-actions float-right">
             <div class="dropdown show">
-                <a href="#" data-toggle="dropdown" data-display="static">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="feather feather-more-horizontal align-middle"
-                    >
-                        <circle cx="12" cy="12" r="1"></circle>
-                        <circle cx="19" cy="12" r="1"></circle>
-                        <circle cx="5" cy="12" r="1"></circle>
-                    </svg>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right">
+
                     <button type="button" onclick="deleteID(';
-                    
-$htmlStarter2=');" class="btn btn-primary col text-center"">Delete</button>                     
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+$DeleteButtonTrigger=');" class="btn btn-warning ">Delete</button>                     
+               ';
+$modalButtonStarter='
+                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#';
+$ModalIDstarter='">
+                    Details
+                    </button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="';
+$ModalIDender='" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Details</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body" style="white-space: break-spaces;">
+                            <p>';
+$ModalButtonCommenter='</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
+            </div>';
+$ModalButtonEnder='    
             </div>
         </div>
     </div>
-    <div class="card-body px-4 pt-2">
+    <div class="card-body px-5 pt-1" style="white-space: break-spaces;">
         <p>';
-$htmlEnder='</p>
+$CommentBoxEnder='</p>
 </div>
 </div>
 </div>';
 $showerPack= new FetchComment();
-    $showerPack->Fetch();
-    foreach( $showerPack->Fetch() as $key=> $shower){
-        echo $htmlStarter1.$key.$htmlStarter2.$shower.$htmlEnder;
+
+    foreach( $showerPack->Fetch() as $commentID=> $commentShow){
+        $randomModalID=substr(str_shuffle(str_repeat($x='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(20/strlen($x)) )),1,20);
+        if(substr_count($commentShow,"\n")>MAX_COMMENTLINE){
+            $startPack = $CommentBoxStarter.$commentID.$DeleteButtonTrigger.$modalButtonStarter.$randomModalID.$ModalIDstarter.$randomModalID.$ModalIDender.$commentShow;
+            $endPack = $ModalButtonCommenter.$ModalButtonEnder.$commentShow.$CommentBoxEnder;
+        }
+        else{
+            $startPack = $CommentBoxStarter.$commentID.$DeleteButtonTrigger;
+            $endPack = $ModalButtonEnder.$commentShow.$CommentBoxEnder;
+        }
+        
+            echo $startPack.$endPack;
     }
-$deleteTest= new DeleteComment();
-//$deleteTest->Delete(113);
 echo '</div>
 </div>
 </body>
