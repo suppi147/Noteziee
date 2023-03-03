5716011791cec3cb3181d872ffaf867f15f277fa
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-03 13:23:30 +0700

    nothin new

--- CommentUI/js/index.js
+++ CommentUI/js/index.js
@@ -1,22 +1,31 @@
 function deleteID (id) {
-    var form = document.createElement('form');
-    form.setAttribute('method', 'post');
-    form.setAttribute('action', 'http://localhost/Noteziee/MainController/MainRail.php');
-    
-    var input=document.createElement('input');
-    input.hidden=true;
-    input.setAttribute('name','id');
-    input.setAttribute('value',id);
-    form.appendChild(input);
-    
-    var input2=document.createElement('input');
-    input2.hidden=true;
-    input2.setAttribute('name','control_flag');
-    input2.setAttribute('value','delete');
-    form.appendChild(input2);
-    
-    
-    document.body.appendChild(form);
-    form.submit();
-    }
-    
+  var form = document.createElement('form');
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', 'http://localhost/Noteziee/MainController/MainRail.php');
+  
+  var input=document.createElement('input');
+  input.hidden=true;
+  input.setAttribute('name','id');
+  input.setAttribute('value',id);
+  form.appendChild(input);
+
+  var input2=document.createElement('input');
+  input2.hidden=true;
+  input2.setAttribute('name','control_flag');
+  input2.setAttribute('value','delete');
+  form.appendChild(input2);
+
+  
+  document.body.appendChild(form);
+  form.submit();
+}
+
+function ReceiveEnter(){
+  var input = document.getElementById("commentContent");
+  input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      document.getElementById("myBtn").click();
+  }
+  });
+}
