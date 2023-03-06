5ddcc3e03010d2aaf5b56a8f368dabc482edbbbf
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-06 16:30:36 +0700

    change delete case and add selectAll case

--- MainController/MainRail.php
+++ MainController/MainRail.php
@@ -3,6 +3,7 @@ include __DIR__.'/../CommentController/CommentController.php';
 include __DIR__.'/../CommentController/PostComment.php';
 include __DIR__.'/../CommentController/DeleteComment.php';
 include __DIR__.'/../CommentController/EditComment.php';
+include __DIR__.'/../CommentController/SelectAllComment.php';
 
 $controller= new CommentController();
 
@@ -10,9 +11,19 @@ if(strcmp($_POST["control_flag"],"post")==0){
      $postTrigger=new PostComment();
      $postTrigger->Post($_POST["commentContent"]);
 }
-else if(strcmp($_POST["control_flag"],"delete")==0){
+else if( strcmp($_POST["control_flag"],"delete")==0 or strcmp($_POST["control_flag"],"select")==0){
      $postTrigger=new DeleteComment();
-     $postTrigger->Delete($_POST["id"]);
+     //$postTrigger->Delete($_POST["id"]);
+     if(strcmp($_POST["control_flag"],"select")==0){
+          $postTrigger->DeleteIDStackUp($_POST["id"],$_POST["checker"]);
+     }
+     else if(strcmp($_POST["control_flag"],"delete")==0){
+          $postTrigger->Delete();
+     }
+}
+else if(strcmp($_POST["control_flag"],"selectAll")==0){
+     $postTrigger=new SelectAllComment();
+     $postTrigger->SelectAllIDStackUp($_POST["checker"]);
 }
 else if(strcmp($_POST["control_flag"],"edit")==0){
      $postTrigger=new EditComment();
