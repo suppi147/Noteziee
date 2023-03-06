46255a74bb66fe2c181d5763dbb96d1a2d4ba5d9
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-06 16:30:15 +0700

    add select control with ajax

--- CommentUI/js/index.js
+++ CommentUI/js/index.js
@@ -29,3 +29,53 @@ function ReceiveEnter(){
   }
   });
 }
+function ToggleOn1(checkID){
+  var checkboxes = document.querySelectorAll('input[id="'+checkID+'"]');
+  if(checkboxes[0].checked==false){
+    checkboxes[0].checked=true;
+  }
+
+}
+function UntoggleOn1(checkID){
+  var checkboxes = document.querySelectorAll('input[id="'+checkID+'"]');
+  if(checkboxes[0].checked==true){
+    checkboxes[0].checked=false;
+  }
+  const xhttp= new XMLHttpRequest();
+  xhttp.open("POST","http://localhost/Noteziee/MainController/MainRail.php",true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("control_flag=select&id="+checkID+"&checker="+checkboxes[0].checked);
+}
+
+function SelectAll() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var allCheckFlag=false;
+    for (var i = 0; i < checkboxes.length; i++) {
+      if(checkboxes[i].checked == true)
+        continue;
+      allCheckFlag=true;
+      break;
+  }
+    for (var i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked == true&&allCheckFlag==true)
+          continue;  
+        if (checkboxes[i].checked == false)
+            checkboxes[i].checked = true;
+        else
+            checkboxes[i].checked = false;
+    }
+
+  const xhttp= new XMLHttpRequest();
+  xhttp.open("POST","http://localhost/Noteziee/MainController/MainRail.php",true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("control_flag=selectAll&checker="+checkboxes[0].checked);
+}
+
+function ChecklistArray(checkID){
+  const xhttp= new XMLHttpRequest();
+  var checkboxes = document.querySelectorAll('input[id="'+checkID+'"]');
+  
+    xhttp.open("POST","http://localhost/Noteziee/MainController/MainRail.php",true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("control_flag=select&id="+checkID+"&checker="+checkboxes[0].checked);
+}
