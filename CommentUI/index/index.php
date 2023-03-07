75e4c5ff9feb11334a6aeec4fcdcc0d026e6d7d7
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-07 21:56:50 +0700

    add strict word limitation

--- CommentUI/index/index.php
+++ CommentUI/index/index.php
@@ -14,13 +14,14 @@
     <body>
         <div class="container">
             <h1 class="h1-edit">Noteziee</h1>
-            <form action="http://localhost/Noteziee/MainController/MainRail.php" method="POST" id="commentID" onclick="ReceiveEnter();">
+            <form action="http://localhost/Noteziee/MainController/MainRail.php" name="submitForm" method="POST" id="commentID" onclick="ReceiveEnter();">
                 <div class="form-group">
-                 <textarea name="commentContent" id="commentContent" class="form-control" placeholder="Enter Comment" rows="10" cols="60"></textarea>
+                 <textarea name="commentContent" id="commentContent" class="form-control" placeholder="Enter Something :3" rows="10" cols="60"></textarea>
                 </div>
                 <div class="form-group">
+                    
                  <input type="hidden" name="control_flag" id="control_flag" value="post" />
-                 <div class="col text-center"><button id="myBtn" type="submit" class="btn btn-primary" >Submit</button>
+                 <div class="col text-center"><button id="myBtn" type="submit" class="btn btn-primary btn-lg" >Submit</button>
                  
                 </div> 
                 </div>
@@ -38,7 +39,9 @@ require_once("EditButton.php");
 require_once("ModalButton.php");
 require_once("CheckBox.php");
 require_once("SelectAllButton.php");
+require_once("CopyButton.php");
 define("MAX_COMMENTLINE",6);
+define("MAX_LETTER",435);
     $showerPack= new FetchComment();
 
     $commentBoxTrigger= new BoxCommentController();
@@ -47,15 +50,37 @@ define("MAX_COMMENTLINE",6);
     $modalButtonTrigger=new ModalButton();
     $checkBoxTrigger=new CheckBox();
     $selectAllButtonTrigger=new SelectAllButton();
+    $copyButtonTrigger=new CopyButton();
 
     foreach( $showerPack->Fetch() as $commentID=> $commentShow){
-        $commentBoxTrigger->AddDeleteCheckBox2Pack($checkBoxTrigger->CheckBoxPack($commentID));
-        $commentBoxTrigger->AddSelectAllButton2Pack($selectAllButtonTrigger->SelectAllButtonPack());
-        $commentBoxTrigger->AddDeleteButton2Pack($deleteButtonTrigger->DeleteButtonPack($commentID));
-        $commentBoxTrigger->AddEditButton2Pack($editButtonTrigger->EditButtonPack($commentShow,$commentID));
-        if(substr_count($commentShow,"\n")>MAX_COMMENTLINE)
-            $commentBoxTrigger->AddModalButton2Pack($modalButtonTrigger->ModalButtonPack($commentShow));       
-        echo $commentBoxTrigger->FullPack($commentShow);
+        $commentBoxTrigger->AddPackHeader($checkBoxTrigger->CheckBoxPack($commentID));
+        $commentBoxTrigger->AddButtons2Pack($selectAllButtonTrigger->SelectAllButtonPack());
+        $commentBoxTrigger->AddButtons2Pack($deleteButtonTrigger->DeleteButtonPack($commentID));
+        $commentBoxTrigger->AddButtons2Pack($editButtonTrigger->EditButtonPack($commentShow,$commentID));
+        $commentBoxTrigger->AddButtons2Pack($copyButtonTrigger->CopyButtonPack($commentID,$commentShow));
+        if(substr_count($commentShow,"\n")>MAX_COMMENTLINE or strlen($commentShow)>MAX_LETTER){
+            $commentBoxTrigger->AddButtons2Pack($modalButtonTrigger->ModalButtonPack($commentShow));
+            if(substr_count($commentShow,"\n")>MAX_COMMENTLINE ){
+                $showMore=explode("\n", $commentShow);
+                $commentShow="";
+                for ($i=0; $i < MAX_COMMENTLINE; $i++) { 
+                    if($i==MAX_COMMENTLINE-1){
+                        $commentShow=$commentShow.$showMore[$i]."\n";
+                        break;
+                    }
+                    $commentShow=$commentShow.$showMore[$i]."\n";
+                }
+                $commentShow=$commentShow.".....";
+            }
+            if(strlen($commentShow)>MAX_LETTER){
+                $commentShow=substr($commentShow,0,MAX_LETTER-100);
+                $commentShow=$commentShow.".....";
+            }
+            
+        }
+        
+        
+        echo $commentBoxTrigger->FullPack($commentShow,$commentID);
     }
 
 echo '</div>
