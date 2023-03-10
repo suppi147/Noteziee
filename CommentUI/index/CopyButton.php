01d8c5ab78b5f3bde9dcc5d9676574c043284656
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-10 19:52:18 +0700

    add copy html decoder

--- CommentUI/index/CopyButton.php
+++ CommentUI/index/CopyButton.php
@@ -6,7 +6,7 @@ class CopyButton{
         $this->copyButton="NULL";
     }
     function CopyButtonPack($checkID,$commentBox){
-        $this->copyButton= '<textarea id="c'.$checkID.'" hidden>'.$commentBox.'</textarea><button class="btn btn-success" onclick="CopyToClipboard(\'#c'.$checkID.'\');tempAlert(\'Copy to clipboard!!!\',1000)">Copy</button>
+        $this->copyButton= '<xmp id="c'.$checkID.'" hidden>'.htmlspecialchars_decode($commentBox).'</xmp><button id="copy" class="btn" onclick="CopyToClipboard(\'#c'.$checkID.'\');tempAlert(\'Copy to clipboard!!!\',1000)">Copy</button>
         
         ';
         return $this->copyButton;
