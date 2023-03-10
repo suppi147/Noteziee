d5c8e9b717ebf9bd33dc34c1fbab7c95b329f84b
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-03-10 19:54:01 +0700

    change index frontend

--- CommentUI/index/index.php
+++ CommentUI/index/index.php
@@ -4,28 +4,61 @@
         <meta charset="utf-8" />
 
         <title>Noteziee ;3</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet" />
         <link href="http://localhost/Noteziee/CommentUI/css/index.css" rel="stylesheet" />
+        <link href="http://localhost/Noteziee/CommentUI/css/checkbox.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
         <script src="http://localhost/Noteziee/CommentUI/js/index.js"></script>
+        
+        <link rel="shortcut icon" href="assets/images/fav.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="http://localhost/Noteziee/Homepage/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/Homepage/assets/css/fontawsom-all.min.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/Homepage/assets/css/button.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/Homepage/assets/plugins/owl/owl.carousel.min.css">
+      <link rel="stylesheet" href="http://localhost/Noteziee/Homepage/assets/plugins/owl/owl.theme.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Noteziee/Homepage/assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <div class="container">
-            <h1 class="h1-edit">Noteziee</h1>
-            <form action="http://localhost/Noteziee/MainController/MainRail.php" name="submitForm" method="POST" id="commentID" onclick="ReceiveEnter();">
+    <header id="menu-jk" class="container-fluid">
+          <div class="container">
+              <div class="row">
+                   <div class="col-lg-3 col-md-12 logo">
+                   <a href="http://localhost/Noteziee/Homepage/"><img class="white" src="http://localhost/Noteziee/Homepage/assets/images/blak.png" alt="" style="margin-top: -50px;"></a>
+                       <a href="http://localhost/Noteziee/Homepage/"><img class="gray" src="http://localhost/Noteziee/Homepage/assets/images/aa.png" alt="" style="margin-top: -10px;"></a>
+
+                       <a class="small-menu" class="d-lg-none" data-toggle="collapse" data-target="#menu" href="#menu" >
+                           <i  class="fas d-lg-none fa-bars"></i>
+                      </a>
+                   </div>
+                   <div id="menu" class="col-lg-9 d-none d-lg-block navigation">
+                       <ul>
+                           <li><a href="http://localhost/Noteziee/Homepage">Home</a></li>
+                           <li><a href="#contact">About Noteziee</a></li>
+                       </ul>
+                   </div>
+              </div>
+          </div>
+      </header>
+      <div class="slider container-fluid">
+      <div class="container">
+      <form action="http://localhost/Noteziee/MainController/MainRail.php" name="submitForm" method="POST" id="commentID" onclick="ReceiveEnter();">
                 <div class="form-group">
-                 <textarea name="commentContent" id="commentContent" class="form-control" placeholder="Enter Something :3" rows="10" cols="60"></textarea>
+                 <textarea name="commentContent" id="commentContent" class="textarea-edit" placeholder="Enter Something :3" rows="10" cols="60" style="margin-top: 40px;"></textarea>
                 </div>
                 <div class="form-group">
-                    
                  <input type="hidden" name="control_flag" id="control_flag" value="post" />
-                 <div class="col text-center"><button id="myBtn" type="submit" class="btn btn-primary btn-lg" >Submit</button>
-                 
+                 <div class="col text-center"><button id="submit" type="submit" onclick="return IsEmpty();" >Submit</button>
                 </div> 
                 </div>
             </form>
+            </div>
+            
             <div class="col text-center" style="padding-bottom:15px;">
             </div>
             <div class="row">
@@ -78,13 +111,98 @@ define("MAX_LETTER",435);
             }
             
         }
-        
+        $commentBoxTrigger->AddButtons2Pack($checkBoxTrigger->CheckBoxPack($commentID));
         
         echo $commentBoxTrigger->FullPack($commentShow,$commentID);
     }
 
 echo '</div>
         </div>
+            </div>
+            <footer id="contact" class="container-fluid">
+       <div class="container">
+
+           <div class="row content-ro">
+             <div class="col-lg-4 col-md-12 footer-form">
+                <h2>About Noteziee</h2>
+
+                <p>Noteziee was created by suppi147 with dedication and hard work. Noteziee is mainly used for noting down small notes like recipes, or as a way to interact between virtual machines with a real machine based on comments and files.</p>
+                  
+             </div>
+
+               <div class="col-lg-4 col-md-12 footer-links">
+                  <div class="row no-margin mt-2">
+                   <h2>Quick Links</h2>
+                   <ul style="margin-left: 60px;">
+                    <li><a style="color:#f7f2f2;" href="#">Home</a></li>
+                    <li><a style="color:#f7f2f2;" href="#features">Features</a></li>
+                    <li><a style="color:#f7f2f2;" href="#contact">About Noteziee</a></li>
+                </ul>
+                   </div>
+                  <div class="row no-margin mt-1">
+                      <h2 class="m-t-2">More Products</h2>
+                    <ul style="margin-left: 60px;">
+                       <li>Catinger</li>
+                       <li>Smart Hi</li>
+                       <li>Smart School</li>
+
+
+                   </ul>
+                  </div>
+
+               </div>
+               <div class="col-lg-4 col-md-12 footer-contact">
+                   <h2>Contact Information</h2>
+                   <div class="address-row">
+                       <div class="icon">
+                           <i class="fas fa-map-marker-alt"></i>
+                       </div>
+                       <div class="detail">
+                           <p style="margin-top: 15px;">HCMC National University Dormitory Zone A</p>
+                       </div>
+                   </div>
+                   <div class="address-row">
+                       <div class="icon">
+                           <i class="far fa-envelope"></i>
+                       </div>
+                       <div class="detail">
+                           <p style="margin-top: 15px;">khoitran2090@gmail.com</p>
+                       </div>
+                   </div>
+                   <div class="address-row">
+                       <div class="icon">
+                           <i class="fas fa-phone"></i>
+                       </div>
+                       <div class="detail">
+                           <p>+91 9751791203 <br> +91 9159669599</p>
+                       </div>
+                   </div>
+               </div>
+
+           </div>
+           <div class="footer-copy">
+               <div class="row">
+                   <div class="col-lg-8 col-md-6">
+                       <p>Copyright © <a>suppi147</a> | All right reserved.</p>
+                   </div>
+                   <div class="col-lg-4 col-md-6 socila-link">
+                       <ul>
+                           <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/suppi147"><i class="fab fa-github"></i></a></li>
+                           <li><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/suppi147"><i class="fab fa-twitter"></i></a></li>
+                           <li><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/khoi.lee.2090/"><i class="fa fa-facebook"></i></a></li>
+                       </ul>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </footer>
+
             </body>
+            <script src="http://localhost/Noteziee/Homepage/assets/js/jquery-3.2.1.min.js"></script>
+            <script src="http://localhost/Noteziee/Homepage/assets/js/popper.min.js"></script>
+            <script src="http://localhost/Noteziee/Homepage/assets/js/bootstrap.min.js"></script>
+            <script src="http://localhost/Noteziee/Homepage/assets/plugins/owl/owl.carousel.min.js"></script>
+            <script src="http://localhost/Noteziee/Homepage/assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+            <script src="http://localhost/Noteziee/Homepage/assets/js/script.js"></script>
                 </html>';
 ?>
