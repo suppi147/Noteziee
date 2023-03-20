5f170222c6eb16ba8843170bb91383cfe6725166
suppi147 <suppi147@dns.suppi147.local>
2023-03-20 20:47:35 +0700

    add email filter

--- UserLogin/LoginController/FilterCredential.php
+++ UserLogin/LoginController/FilterCredential.php
@@ -6,13 +6,20 @@ class FilterCredential extends LoginController{
     function __construct(){
         parent::__construct();
     }
+
+    function FilterEmail(){
+        if(!(filter_var($this->username, FILTER_VALIDATE_EMAIL))){
+            echo 'email invalid';
+            exit();
+        }
+    }
     function FilterEmailPassword($username,$password){
         $characterFilterEmail= new FilterComment();
         $characterFilterPassword= new FilterComment();
         if($characterFilterEmail->FilterComment($username) && $characterFilterPassword->FilterComment($password)){
             $this->username = $characterFilterEmail->GetItemfiltering();
-            $this->password = $characterFilterPassword->GetItemfiltering();
-            
+            $this->FilterEmail();
+            $this->password = $characterFilterPassword->GetItemfiltering();   
         }
     }
     
