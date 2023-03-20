0da881ee01aa90c1d2cc9faa186b04471c40ec62
suppi147 <suppi147@dns.suppi147.local>
2023-03-20 22:06:30 +0700

    check for record exist and update if not

--- UserLogin/LoginController/UpdateCredential.php
+++ UserLogin/LoginController/UpdateCredential.php
@@ -2,20 +2,31 @@
 require_once("FilterCredential.php");
 require_once("LoginController.php");
 class UpdateCredential extends LoginController{
-   
-
     function __construct(){
         parent::__construct();
     }
 
-    
     function Insert2LoginDB($username,$password){
+
         $filter= new FilterCredential();
         $filter->FilterEmailPassword($username,$password);
         $this->username=$filter->GetUsername();
         $this->password=$filter->GetPassword();
+        
+        if($filter->CheckRecordExist()){
+            echo 'record exist';
+        }
+        else{
+            $this->Connect2loginDB();
+
+            $query='INSERT INTO users(username,password) VALUES ("'.$this->username.'","'.$this->password.'")';
+            $this->InteractCommentDB->Update2DB($query);
+
+            $this->Disconnect2loginDB();
+        }
+            
+        
     }
 
 }
-
 ?>
