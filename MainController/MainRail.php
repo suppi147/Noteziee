6b252de5ff81957c03c3565f8ffee2b3f309cc30
suppi147 <suppi147@dns.suppi147.local>
2023-03-21 16:41:36 +0700

    add direct login post without mainrail

--- MainController/MainRail.php
+++ MainController/MainRail.php
@@ -5,7 +5,6 @@ include __DIR__.'/../CommentController/DeleteComment.php';
 include __DIR__.'/../CommentController/EditComment.php';
 include __DIR__.'/../CommentController/SelectAllComment.php';
 
-include __DIR__.'/../UserLogin/LoginController/LoginController.php';
 
 $controller= new CommentController();
 
@@ -31,11 +30,8 @@ else if(strcmp($_POST["control_flag"],"edit")==0){
      $postTrigger=new EditComment();
      $postTrigger->Edit($_POST["commentContent"],$_POST["id"]);
 }
-else if(strcmp($_POST["control_flag"],"login")==0){
-     $postTrigger=new EditComment();
-     $postTrigger->Edit($_POST["commentContent"],$_POST["id"]);
-}
 else{
+     
      echo '<script>alert("Unknown control flag")</script>';
 }
 header("Location: ".$controller->indexLocation);
