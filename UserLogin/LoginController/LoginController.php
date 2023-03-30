d046dce744624800ebb1cd77044bb724896e5341
suppi147 <suppi147@dns.suppi147.local>
2023-03-30 22:11:06 +0700

    add session interact and change db set mechanism

--- UserLogin/LoginController/LoginController.php
+++ UserLogin/LoginController/LoginController.php
@@ -1,13 +1,17 @@
 <?php
 include __DIR__.'/../../DatabaseInteraction/DatabaseController.php';
+
+//include __DIR__.'/../../DatabaseInteraction/UpdateDatabase.php';
 class LoginController{
     protected $InteractCommentDB;
     protected $username;
     protected $password;
+    protected $session;
     function __construct(){
         $this->username="NULL";
         $this->password="NULL";
-        $this->InteractCommentDB= new  DatabaseController();
+        $this->InteractCommentDB= new DatabaseController();
+        $this->session= new SessionManager();
     }
     function GetUsername(){
         return $this->username;
@@ -23,13 +27,11 @@ class LoginController{
         $dbname="loginDB";
         $username="loginuser";
         $password="password";
-        $this->InteractCommentDB->Connect2DB($hostname,$dbname,$username,$password);
+        $this->InteractCommentDB->SetDBInformation($hostname,$dbname,$username,$password);
+        $this->InteractCommentDB->Connect2DB();
     }
     function Disconnect2loginDB(){
         $this->InteractCommentDB->Disconnect2DB();
     }
-
-    
-
 }
 ?>
