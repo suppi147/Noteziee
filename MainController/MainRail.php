5d983b329738ca92b1be822061807d7aac2a962f
suppi147 <suppi147@dns.suppi147.local>
2023-03-30 22:20:05 +0700

    add user's in4 for separate interaction with unique table

--- MainController/MainRail.php
+++ MainController/MainRail.php
@@ -5,16 +5,18 @@ include __DIR__.'/../CommentController/DeleteComment.php';
 include __DIR__.'/../CommentController/EditComment.php';
 include __DIR__.'/../CommentController/SelectAllComment.php';
 
-
+session_start();
 $controller= new CommentController();
 
 if(strcmp($_POST["control_flag"],"post")==0){
      $postTrigger=new PostComment();
+     $postTrigger->InteractCommentDB->SetDBInformation("localhost","CommentDB",$_SESSION['username'],$_SESSION['password']);
      $postTrigger->Post($_POST["commentContent"]);
 }
 else if( strcmp($_POST["control_flag"],"delete")==0 or strcmp($_POST["control_flag"],"select")==0){
      $postTrigger=new DeleteComment();
      //$postTrigger->Delete($_POST["id"]);
+     $postTrigger->InteractCommentDB->SetDBInformation("localhost","CommentDB",$_SESSION['username'],$_SESSION['password']);
      if(strcmp($_POST["control_flag"],"select")==0){
           $postTrigger->DeleteIDStackUp($_POST["id"],$_POST["checker"]);
      }
@@ -24,10 +26,12 @@ else if( strcmp($_POST["control_flag"],"delete")==0 or strcmp($_POST["control_fl
 }
 else if(strcmp($_POST["control_flag"],"selectAll")==0){
      $postTrigger=new SelectAllComment();
+     $postTrigger->InteractCommentDB->SetDBInformation("localhost","CommentDB",$_SESSION['username'],$_SESSION['password']);
      $postTrigger->SelectAllIDStackUp($_POST["checker"]);
 }
 else if(strcmp($_POST["control_flag"],"edit")==0){
      $postTrigger=new EditComment();
+     $postTrigger->InteractCommentDB->SetDBInformation("localhost","CommentDB",$_SESSION['username'],$_SESSION['password']);
      $postTrigger->Edit($_POST["commentContent"],$_POST["id"]);
 }
 else{
