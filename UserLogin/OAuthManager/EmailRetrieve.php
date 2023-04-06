9391f37c858febd75f3bdb91554aa1feab304752
suppi147 <suppi147@dns.suppi147.local>
2023-04-06 21:33:21 +0700

    add basic OAuth

---
+++ UserLogin/OAuthManager/EmailRetrieve.php
@@ -0,0 +1,28 @@
+<?php
+include __DIR__.'/../UpdateCredential.php';
+require_once('vendor/autoload.php');
+
+
+$clientID='452812593294-9rmutaejqhrupe95tp5gd06jucv17u15.apps.googleusercontent.com';
+$clientSecret='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';
+$redirectURI='http://localhost/Noteziee/UserLogin/LoginController/OAuthManager/EmailRetrieve.php';
+
+$client= new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectURI);
+$client->addScope('profile');
+$client->addScope('email');
+
+if($_GET['code']){
+    $token=$client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token);
+
+    $gauth= new Google_Service_Oauth2($client);
+    $googleAccountIn4= $gauth->userinfo->get();
+
+    $email=$googleAccountIn4->email;
+    $a =new UpdateCredential();
+    $a->Insert2LoginDB($email);
+}
+?>
---
+++ UserLogin/OAuthManager/index.php
@@ -0,0 +1,62 @@
+<?php
+require_once('vendor/autoload.php');
+
+$clientID='452812593294-9rmutaejqhrupe95tp5gd06jucv17u15.apps.googleusercontent.com';
+$clientSecret='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';
+$redirectURI='http://localhost/Noteziee/UserLogin/LoginController/OAuthManager/EmailRetrieve.php';
+
+$client= new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectURI);
+$client->addScope('profile');
+$client->addScope('email');
+
+
+?>
+
+
+<html>
+<head>
+  <title>Noteziee</title>
+  <link rel="shortcut icon" href="http://localhost/Noteziee/Homepage/assets/icon/icon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="http://localhost/Noteziee/UserLogin/assets/css/login.css">
+  <meta charset="utf-8">
+ 
+</head>
+
+<body>
+  <div class="login-root">
+    <div class="box-root flex-flex flex-direction--column" style="min-height: 100vh;flex-grow: 1;">
+      <div class="box-root padding-top--24 flex-flex flex-direction--column" style="flex-grow: 1; z-index: 9;">
+        <div class="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
+          <div class="form-center">
+            <a class="a" href="http://localhost/Noteziee/Homepage/" rel="dofollow">
+            <img src="http://localhost/Noteziee/UserLogin/assets/images/tree.png" alt=""></a>
+       </div>
+        </div>
+        <div class="formbg-outer">
+        <div class="row">
+            
+           <div  class="formbg col-md-7 text-part" >
+            <div class="formbg-inner padding-horizontal--48">
+              <span class="padding-bottom--15">Sign in to your account</span>
+              <form action=<?php echo $client->createAuthUrl();?> name="loginForm" method="POST" id="loginID">      
+                <div class="field padding-bottom--24">
+                  <input type="submit" name="control_flag" value="Google Account">
+                </div>
+              </form>
+            </div>
+        </div>
+        
+        
+          </div>
+          <div class="footer-link padding-top--24">
+            <span style="color: #f2f2f2;">I know you are questioning why I use Oauth!? Well, nothing personal, It's just me liking It.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
