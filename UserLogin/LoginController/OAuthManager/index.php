a9d7810e10a23933dd0a65da4320fe7d43865501
suppi147 <suppi147@dns.suppi147.local>
2023-04-08 08:34:09 +0700

    add OAuthManager control client var

--- UserLogin/LoginController/OAuthManager/index.php
+++ UserLogin/LoginController/OAuthManager/index.php
@@ -1,18 +1,5 @@
 <?php
-require_once('vendor/autoload.php');
-
-$clientID='452812593294-9rmutaejqhrupe95tp5gd06jucv17u15.apps.googleusercontent.com';
-$clientSecret='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';
-$redirectURI='http://localhost/Noteziee/UserLogin/LoginController/OAuthManager/EmailRetrieve.php';
-
-$client= new Google_Client();
-$client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectURI);
-$client->addScope('profile');
-$client->addScope('email');
-
-
+require_once('OAuthManager.php');
 ?>
 
 
