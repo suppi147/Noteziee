40084a452191fbfe0ce35edc9d30a5937c59601e
khoitran2090@gmail.com <khoitran2090@gmail.com>
2023-04-20 21:40:04 +0700

    add limit account warning

---
+++ SomethingGoesWrong/PolicyViolation/AccountLimitCross/index.php
@@ -0,0 +1,41 @@
+<?php http_response_code(404); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Noteziee</title>
+    <link rel="shortcut icon" href="http://localhost/Noteziee/Homepage/assets/icon/icon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="http://localhost/Noteziee/SomethingGoesWrong/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/SomethingGoesWrong/assets/css/fontawsom-all.min.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/SomethingGoesWrong/assets/css/button.css">
+    <link rel="stylesheet" href="http://localhost/Noteziee/SomethingGoesWrong/assets/scss/404.scss">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Noteziee/SomethingGoesWrong/assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+
+<main>
+    <div class="container">
+      <div class="row">
+        <div data-aos="fade-left"  class="col-md-5 image-part">
+            <img src="http://localhost/Noteziee/SomethingGoesWrong/assets/images/404s.png" alt="">
+       </div>
+       <div data-aos="fade-left"  class="col-md-7 text-part">
+        <h1>Violation</h1>
+        <h2>UH NOOO! Green card.</h2>
+        <p>We have just got the fifty accounts signed up which is full(Its a free server I got from a drug dealer... What would you expect!!) and yep, we our of the service. For further informations please contact suppi147 by the button below.
+        </p>
+        <a href="https://www.facebook.com/khoi.lee.2090" target="_blank"><button id="circle" style="margin-top: 20px;">Facebook</button></a>
+      </div>
+        </div>
+        
+      </div>
+    </div>
+  </main>
+</body>
+  </html>
